961f1b85ed00c673c966bb9e22d762e10133889c
Roberts Kalnins <rkalnins314@gmail.com>
2019-07-14 18:40:21 -0400

    Move sd controls to Controls

--- src/main/kotlin/frc/team2767/healthchecktest/control/Controls.kt
+++ src/main/kotlin/frc/team2767/healthchecktest/control/Controls.kt
@@ -1,5 +1,14 @@
 package frc.team2767.healthchecktest.control
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import frc.team2767.healthchecktest.command.HealthCheckCommand
+
 class Controls {
     private val SmartdashboardControls = SmartdashboardControls()
+}
+
+class SmartdashboardControls {
+    init {
+        SmartDashboard.putData("Healthcheck", HealthCheckCommand())
+    }
 }
