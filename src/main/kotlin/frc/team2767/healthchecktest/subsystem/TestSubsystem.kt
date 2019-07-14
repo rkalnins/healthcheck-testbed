29616628f6a35964de1583c434ea243b306136f1
Roberts Kalnins <rkalnins314@gmail.com>
2019-07-14 16:29:24 -0400

    Cleanup

--- src/main/kotlin/frc/team2767/healthchecktest/subsystem/TestSubsystem.kt
+++ src/main/kotlin/frc/team2767/healthchecktest/subsystem/TestSubsystem.kt
@@ -2,7 +2,6 @@ package frc.team2767.healthchecktest.subsystem
 
 import edu.wpi.first.wpilibj.command.Subsystem
 
-@Singleton
 class TestSubsystem : Subsystem() {
     override fun initDefaultCommand() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
